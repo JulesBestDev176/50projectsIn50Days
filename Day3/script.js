a9bb5273364e706def1fb92d362dfe51b219d20d
JulesBestDev176 <souleymanefall176@gmail.com>
2024-08-26 00:52:41 +0000

    Day 3 update

--- Day3/script.js
+++ Day3/script.js
@@ -1,15 +1,16 @@
 const container = document.querySelector(".container");
 const cercle = document.querySelector(".cercle");
-const menus = document.querySelector(".menus");
+const menu = document.querySelector(".menu");
 
 cercle.addEventListener("click", (e) => {
+  e.preventDefault();
   if (container.classList.contains("rotatePage")) {
     container.classList.remove("rotatePage");
     cercle.classList.remove("rotateCercle");
-    menus.classList.remove("menuAvant");
+    menu.classList.remove("menuAvant");
   } else {
     container.classList.add("rotatePage");
     cercle.classList.add("rotateCercle");
-    menus.classList.add("menuAvant");
+    menu.classList.add("menuAvant");
   }
 });
